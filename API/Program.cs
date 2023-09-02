328f0329bc19aa0287389982db4cf152c23e71e8
Roman Bogoudinov <r.bogoudinov@mac.com>
2023-09-02 11:56:40 +0300

    fetching products from api

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -22,6 +23,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(opt => {
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:5173");
+});
 
 app.UseHttpsRedirection();
 
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([
+    { name: "product1", price: 100.0 },
+    { name: "product2", price: 200.0 },
+  ]);
+
+  useEffect(() => {
+    fetch ('http://localhost:5000/api/products')
+    .then(resp => resp.json())
+    .then(data => setProducts(data))
+  },[])
+
+  function addProduct() {
+    setProducts((prevState) => [
+      ...prevState,
+      {
+        name: "product" + (prevState.length + 1),
+        price: prevState.length * 100 + 100.0,
+      },
+    ]);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <h1>Ecom store</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>
+            {item.name} - {item.price} €
+          </li>
+        ))}
+      </ul>
+      <button onClick={addProduct}> Add product</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
