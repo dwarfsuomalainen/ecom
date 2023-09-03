60647a6bb77148f89ba8e070817f2e84a4199a8e
Roman Bogoudinov <r.bogoudinov@mac.com>
2023-09-03 21:33:07 +0300

    theme mui, 'dark mode' added

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,18 +1,35 @@
-import { CssBaseline, Container } from "@mui/material";
+import {
+  CssBaseline,
+  Container,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
 import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
+import { dark } from "@mui/material/styles/createPalette";
+import { useState } from "react";
 
 function App() {
-  
-  
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? "dark" : "light";
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+    },
+  });
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
 
   return (
-    <>
-    <CssBaseline></CssBaseline>  {/* baseline for toolbar */}
-      <Header></Header>
-      <Container><Catalog/></Container>
-      
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline></CssBaseline> {/* baseline for toolbar */}
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}></Header>
+      <Container>
+        <Catalog />
+      </Container>
+    </ThemeProvider>
   );
 }
 
