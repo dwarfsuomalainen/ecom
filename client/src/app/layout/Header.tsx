37b5526eeddfc0bfffb77930d235a5f9e1667254
Roman Bogoudinov <r.bogoudinov@mac.com>
2023-09-05 08:24:03 +0300

    navbar added, react-router

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,19 +1,80 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import { NavLink } from "react-router-dom";
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  Switch,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
+const midLinks = [
+  { title: "catalog", path: "/catalog" },
+  { title: "about", path: "/about" },
+  { title: "contacts", path: "/contacts" },
+];
+
+const rightLinks = [
+  { title: "login", path: "/catalog" },
+  { title: "register", path: "/register" },
+];
+
+const navStyles = {
+  color: "inherit",
+  typography: "h6",
+  "&:hover": { color: "gray.500" },
+  "&.active": { color: "text.secondary" },
+  textDecoration: "none",
+};
 interface Props {
-    darkMode: boolean;
-    handleThemeChange: () => void;
+  darkMode: boolean;
+  handleThemeChange: () => void;
 }
 
-export default function Header ({darkMode, handleThemeChange}: Props){
-    return (
-        <AppBar position="static" sx={{mb:4}}>
-            <Toolbar>
-                <Typography variant="h6">
-                    Ski equipment web store
-                </Typography>
-                <Switch checked={darkMode} onChange={handleThemeChange} />
-            </Toolbar>
-        </AppBar>
-    )
+export default function Header({ darkMode, handleThemeChange }: Props) {
+  return (
+    <AppBar position="static" sx={{ mb: 4 }}>
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Box display='flex' alignItems='center'>
+          {" "}
+          <Typography variant="h6" component={NavLink} to="/" sx={navStyles}>
+            Ski equipment web store
+          </Typography>
+          <Switch checked={darkMode} onChange={handleThemeChange} />
+        </Box>
+
+        <List sx={{ display: "flex" }}>
+          {midLinks.map(({ title, path }) => (
+            <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
+        <Box alignItems='center'>
+          <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
+            <Badge badgeContent="4" color="secondary">
+              <ShoppingCart></ShoppingCart>
+            </Badge>
+          </IconButton>
+          <List sx={{ display: "flex" }}>
+            {rightLinks.map(({ title, path }) => (
+              <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
