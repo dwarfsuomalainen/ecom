8b2b6263cf18d47f13e88cd91d27347acf42abb7
Roman Bogoudinov <r.bogoudinov@mac.com>
2023-09-08 17:00:37 +0300

    error handling

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -9,28 +9,43 @@ namespace API.Controllers
 {
     public class BuggyController
     {
+        public ActionResult Unauthorized { get; private set; }
+        public object ModelState { get; private set; }
+
         [HttpGet("not-found")]
         public ActionResult GetNotFound()
         {
             return NotFound();
         }
+
+        private ActionResult NotFound()
+        {
+            throw new NotImplementedException();
+        }
+
         [HttpGet("bad-request")]
         public ActionResult GetBadRequest()
         {
             return BadRequest("Bad request");
         }
+
+        private ActionResult BadRequest(string v)
+        {
+            return BadRequest("Bad request");
+        }
+
         [HttpGet("unauthorized")]
         public ActionResult GetUnauthorized()
         {
-            return Unauthorized();
+            return Unauthorized;
         }
         [HttpGet("validation-error")]
-        public ActionResult GetValidationError()
-        {
-            ModelState.AddModelError("Problem1", "This is the first error");
-            ModelState.AddModelError("Problem2", "This is the second error");
-            return ValidationProblem();
-        }
+        // public ActionResult GetValidationError()
+        // {
+        //     ModelState.AddModelError("Problem1", "This is the first error");
+        //     ModelState.AddModelError("Problem2", "This is the second error");
+        //     return ValidationProblem();
+        // }
         [HttpGet("server-error")]
         public ActionResult GetServerError()
         {
