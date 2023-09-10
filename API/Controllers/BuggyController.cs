edd8c2fc19f825a52e0657591efb38fad6a43f4f
Roman Bogoudinov <r.bogoudinov@mac.com>
2023-09-10 21:22:12 +0300

    buggy controller error

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -9,47 +9,47 @@ namespace API.Controllers
 {
     public class BuggyController
     {
-        public ActionResult Unauthorized { get; private set; }
-        public object ModelState { get; private set; }
+        // public ActionResult Unauthorized { get; private set; }
+        // public object ModelState { get; private set; }
 
-        [HttpGet("not-found")]
-        public ActionResult GetNotFound()
-        {
-            return NotFound();
-        }
-
-        private ActionResult NotFound()
-        {
-            throw new NotImplementedException();
-        }
-
-        [HttpGet("bad-request")]
-        public ActionResult GetBadRequest()
-        {
-            return BadRequest("Bad request");
-        }
-
-        private ActionResult BadRequest(string v)
-        {
-            return BadRequest("Bad request");
-        }
-
-        [HttpGet("unauthorized")]
-        public ActionResult GetUnauthorized()
-        {
-            return Unauthorized;
-        }
-        [HttpGet("validation-error")]
-        // public ActionResult GetValidationError()
+        // [HttpGet("not-found")]
+        // public ActionResult GetNotFound()
         // {
-        //     ModelState.AddModelError("Problem1", "This is the first error");
-        //     ModelState.AddModelError("Problem2", "This is the second error");
-        //     return ValidationProblem();
+        //     return NotFound();
         // }
-        [HttpGet("server-error")]
-        public ActionResult GetServerError()
-        {
-            throw new Exception ("Server error");
-        }
+
+        // private ActionResult NotFound()
+        // {
+        //     throw new NotImplementedException();
+        // }
+
+        // [HttpGet("bad-request")]
+        // public ActionResult GetBadRequest()
+        // {
+        //     return BadRequest("Bad request");
+        // }
+
+        // private ActionResult BadRequest(string v)
+        // {
+        //     return BadRequest("Bad request");
+        // }
+
+        // [HttpGet("unauthorized")]
+        // public ActionResult GetUnauthorized()
+        // {
+        //     return Unauthorized;
+        // }
+        // [HttpGet("validation-error")]
+        // // public ActionResult GetValidationError()
+        // // {
+        // //     ModelState.AddModelError("Problem1", "This is the first error");
+        // //     ModelState.AddModelError("Problem2", "This is the second error");
+        // //     return ValidationProblem();
+        // // }
+        // [HttpGet("server-error")]
+        // public ActionResult GetServerError()
+        // {
+        //     throw new Exception ("Server error");
+        // }
     }
 }
